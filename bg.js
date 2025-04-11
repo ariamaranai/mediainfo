bee4b968e71da322e2c87899d4d8bad17e7633a1
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-12 00:04:46 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -6,49 +6,53 @@ chrome.runtime.onInstalled.addListener(() =>
     documentUrlPatterns: ["https://*/*", "http://*/*", "file://*"]
   })
 );
-chrome.contextMenus.onClicked.addListener((info, {id: tabId}) => {
+chrome.contextMenus.onClicked.addListener((info, tab) => {
     chrome.action.setPopup({
       popup: "popup.htm",
-      tabId
+      tabId: tab.id
     }, () => {
-      chrome.action.openPopup(() => {
-        if (info.mediaType == "image") {
-          let url = info.srcUrl;
-          fetch (url)
-            .then(r => Promise.all([r.clone().blob(), r.bytes(), r.headers.get("content-type").slice(6)]))
-              .then(r => Promise.all([createImageBitmap(r[0]), r[1].length.toLocaleString("en-US"), r[2]]))
-                .then(r => chrome.runtime.sendMessage(
+      if (info.mediaType == "image") {
+        let url = info.srcUrl;
+        fetch (url)
+          .then(r => Promise.all([r.clone().blob(), r.bytes(), r.headers.get("content-type").slice(6)]))
+            .then(r => Promise.all([createImageBitmap(r[0]), r[1].length.toLocaleString("en-US"), r[2]]))
+              .then(r =>
+                chrome.action.openPopup(() => chrome.runtime.sendMessage(
                   "url: <a target=_blank href=" + url + ">" + url + "</a>\nsize: " +
                   r[0].width +  " x " + r[0].height + "\nfilesize: " +  r[1] +  " bytes\nformat: " + r[2]
-                )).catch(() => 0);
-        } else
-          chrome.userScripts.execute({
-            target: { frameIds: [info.frameId], tabId},
-            js: [{ code:
+                ))
+              ).catch(() => 0);
+      } else
+        chrome.userScripts.execute({
+          target: { tabId: tab.id },
+          js: [{ code:
 `(() => {
   let d = document;
   let video = d.body.getElementsByTagName("video");
   let i = video.length;
-  if (d.head.childElementCount != 1) {
-    let index = 0;
-    if (i > 1) {
-      let maxWidth = 0;
-      let width = 0;
-      while (
-        maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-        i
-      );
-    }
-    video = video[index];
-  } else
-    video = video[0];
+  if (i) {
+    if (d.head.childElementCount != 1) {
+      let index = 0;
+      if (i > 1) {
+        let maxWidth = 0;
+        let width = 0;
+        while (
+          maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+          i
+        );
+      }
+      video = video[index];
+    } else
+      video = video[0];
     let src = video.currentSrc;
-  return "url: <a target=_blank href=" + src + ">" + src + "</a>\nsize: " + video.videoWidth + " x " + video.videoHeight;
+    return "url: <a target=_blank href=" + src + ">" + src + "</a>\\nsize: " + video.videoWidth + " x " + video.videoHeight;
+  }
 })();`
-            }]
-          }).then(results =>
-            (results &&= results[0].result) && chrome.runtime.sendMessage(results)
-          ).catch(() => 0);
-    });
+          }]
+        }).then(results =>
+          (results &&= results[0].result) && chrome.action.openPopup(() =>
+            chrome.runtime.sendMessage(results)
+          )
+        ).catch(() => 0);
   });
 });
