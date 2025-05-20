51aa4d0017d91b99884b3aee486690252cdcd924
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-20 20:31:41 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,46 +1,31 @@
-chrome.runtime.onInstalled.addListener(() =>
-  chrome.contextMenus.create({
-    id: "",
-    title: "View info",
-    contexts: ["page", "frame", "image", "video"],
-    documentUrlPatterns: ["https://*/*", "http://*/*", "file://*"]
-  })
-);
-chrome.contextMenus.onClicked.addListener((info, { id: tabId, windowId }) =>
-    chrome.action.setPopup({
-      popup: "popup.htm",
-      tabId
-    }, () => {
-      if (info.mediaType == "image") {
-        let url = info.srcUrl;
-        fetch (url)
-          .then(r => Promise.all([r.clone().blob(), r.bytes(), r.headers.get("content-type").slice(6)]))
-            .then(r => Promise.all([createImageBitmap(r[0]), r[1].length.toLocaleString("en-US"), r[2]]))
-              .then(r => (
-                chrome.action.openPopup(() => chrome.runtime.sendMessage(
-                  "url: <a target=_blank href=" + url + ">" + url + "</a>\nsize: " +
-                  r[0].width +  " x " + r[0].height + "\nfilesize: " +  r[1] +  " bytes\nformat: " + r[2],
-                )),
-                chrome.action.setPopup({
-                  popup: "",
-                  tabId
-                })
-              )
-          ).catch(() => 0);
-      } else
-        chrome.windows.get(windowId, async window => {
-          window.state == "fullscreen" &&
-          await chrome.windows.update(windowId, {
-            state: "maximized"
-          });
-          try {
-            let { frameId } = info;
-            let results = (await chrome.userScripts.execute({
-              target: frameId ? { tabId, frameIds: [frameId] } : { tabId, allFrames: !0 },
-              js: [{ code:
+chrome.contextMenus.onClicked.addListener(async (info, { id: tabId, windowId }) => {
+  chrome.action.setPopup({ popup: "popup.htm", tabId });
+  try {
+    (await chrome.windows.get(windowId)).state == "fullscreen" && await chrome.windows.update(windowId, { state: "maximized" });
+    if (info.mediaType == "image") {
+      let url = info.srcUrl;
+      let res = await fetch (url);
+      let format = res.headers.get("content-type");
+      let b = (await res.clone().bytes()).length;
+      let localeFilesize = b.toLocaleString("en-US");
+      let bmp = await createImageBitmap(await res.blob());
+      chrome.action.openPopup(() =>
+        chrome.runtime.sendMessage(
+          "<a target=_blank href=" + url + ">" + url + "</a>\n" + bmp.width + " x " + bmp.height + "\n" + (
+            b > 1023 && b < 1073741824
+              ? (b < 1048576 ? (b / 1024).toFixed(1) + " KB (" : (b / 1048576).toFixed(1) + " MB (") + localeFilesize + " bytes)\n"
+              : localeFilesize + "Bytes\n"
+          ) + format
+        )
+      );
+    } else {
+      let { frameId } = info;
+      let results =  await chrome.userScripts.execute({
+        target: frameId ? { tabId, frameIds: [frameId] } : { tabId, allFrames: !0 },
+        js: [{ code:
 `(() => {
   let d = document;
-  let video = d.body.getElementsByTagName("video");
+  let video = document.body.getElementsByTagName("video");
   let i = video.length;
   if (i) {
     let index = 0;
@@ -53,20 +38,57 @@ chrome.contextMenus.onClicked.addListener((info, { id: tabId, windowId }) =>
       );
     }
     let src = (video = video[index]).currentSrc;
-    return "url: <a target=_blank href=" + src + ">" + src + "</a>\\nsize: " + video.videoWidth + " x " + video.videoHeight + "\\n";
+    return [video.videoWidth, video.videoHeight, src];
   }
 })();`
-                }]
+        }]
+      });
+      if (results) {
+        let i = results.length;
+        let result;
+        let r = [0];
+        while (
+          (result = results[--i].result) && r[0] < result[0] && (r = result), 
+          i
+        );
+        if (r.length > 1) {
+          let url = r[2];
+          let html = "<a target=_blank href=" + url + ">" + url + "</a>\n" + r[0] + " x " + r[1];
+          url[0] != "b"
+            ? await new Promise(resolve => {
+                let f = item => {
+                  if (item.url == url) {
+                    chrome.downloads.cancel(item.id);
+                    chrome.downloads.onCreated.removeListener(f);
+                    let b = item.totalBytes;
+                    let localeFilesize = b.toLocaleString("en-US");
+                    chrome.action.openPopup(() =>
+                      chrome.runtime.sendMessage(
+                        html + "\n" + (
+                          b > 1023 && b < 1099511627775
+                            ? (b < 1048576 ? (b / 1024).toFixed(1) + " KB (" : b < 1073741824 ? (b / 1048576).toFixed(1) + " MB (" : (b / 1073741824).toFixed(1)  + " GB (") + localeFilesize + " bytes)"
+                            : localeFilesize + "Bytes"
+                          )
+                        )
+                      );
+                    resolve();
+                  }
+                }
+                chrome.downloads.onCreated.addListener(f);
+                chrome.downloads.download({ url });
               })
-            );
-            (results &&= results.reduce((a, v) => a + (v.result || ""), "")) && (
-              chrome.action.openPopup(() => chrome.runtime.sendMessage(results.slice(0, -1))),
-              chrome.action.setPopup({
-                popup: "",
-                tabId
-              })
-            )
-          } catch (e) {}
-        })
+            : chrome.action.openPopup(() => chrome.runtime.sendMessage(html));
+        }
+      }
+    }
+  } catch (e) {}
+  chrome.action.setPopup({ popup: "", tabId });
+});
+chrome.runtime.onInstalled.addListener(() =>
+  chrome.contextMenus.create({
+    id: "",
+    title: "View info",
+    contexts: ["page", "frame", "image", "video"],
+    documentUrlPatterns: ["https://*/*", "http://*/*", "file://*"]
   })
 );
